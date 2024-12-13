94238b03d9eede0364783008b76350b8738c701e
Marius Vizbras <marius.viz@gmail.com>
2024-12-13 15:40:40 +0200

    Patient CRUD

---
+++ src/Form/PatientType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use App\Entity\Patient;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\LessThanOrEqual;
+
+class PatientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('type')
+            ->add('gender', ChoiceType::class, [
+                'choices' => [
+                    'Male' => 'male',
+                    'Female' => 'female',
+                    'Other' => 'other',
+                ],
+                'placeholder' => 'Choose a gender',
+                'required' => true,
+            ])
+            ->add('age', null, [
+                'widget' => 'single_text',
+                'constraints' => [
+                    new LessThanOrEqual([
+                        'value' => 'today', // Ensure the date is today or in the past
+                        'message' => 'The date of birth cannot be in the future.',
+                    ]),
+                ],
+            ])
+            ->add('markingNumber')
+            ->add('passportNumber')
+            ->add('appearance')
+            ->add('client', EntityType::class, [
+                'class' => Client::class,
+                'choice_label' => function (Client $client) {
+                    return $client->getName() . ' ' . $client->getLastName();
+                },
+                'placeholder' => 'Select an owner',
+                'required' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Patient::class,
+        ]);
+    }
+}
