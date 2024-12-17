62e013295d6a974c6c4874b7691fadb29c89fee7
Marius Vizbras <marius.viz@gmail.com>
2024-12-17 21:52:40 +0200

    Form Events, PRE_SET, PRE_SUBMIT

--- src/Form/PatientType.php
+++ src/Form/PatientType.php
@@ -8,16 +8,68 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Constraints\LessThanOrEqual;
 
 class PatientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('name')
-            ->add('type')
+        $builder->add('client', EntityType::class, [
+            'class' => Client::class,
+            'choice_label' => function (Client $client) {
+                return $client->getName().' '.$client->getLastName();
+            },
+            'placeholder' => 'Select an owner',
+            'required' => true,
+            'constraints' => [
+                new Assert\NotBlank([
+                    'message' => 'Please select an owner before filling the form.',
+                ]),
+            ],
+        ]);
+
+        //  PRE_SET_DATA
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+            $data = $event->getData();
+
+            // ar klientas nustatytas formoj?
+            $clientSelected = $data instanceof Patient && null !== $data->getClient();
+
+            // fileds priklausomai nuo cliento
+            $this->addFields($form, !$clientSelected);
+        });
+
+        // Form event listener PRE_SUBMIT
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            $form = $event->getForm();
+            $data = $event->getData();
+
+            if (is_array($data) && isset($data['client'])) {
+                $clientSelected = !empty($data['client']);
+            } else {
+                $clientSelected = false;
+            }
+
+            $this->addFields($form, !$clientSelected);
+        });
+    }
+
+    private function addFields(FormInterface $form, bool $disabled): void
+    {
+        $form
+            ->add('name', null, [
+                'disabled' => $disabled,
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Name is required.']),
+                ],
+            ])
+            ->add('type', null, ['disabled' => $disabled])
             ->add('gender', ChoiceType::class, [
                 'choices' => [
                     'Male' => 'male',
@@ -25,10 +77,12 @@ class PatientType extends AbstractType
                     'Other' => 'other',
                 ],
                 'placeholder' => 'Choose a gender',
+                'disabled' => $disabled,
                 'required' => true,
             ])
             ->add('age', null, [
                 'widget' => 'single_text',
+                'disabled' => $disabled,
                 'constraints' => [
                     new LessThanOrEqual([
                         'value' => 'today',
@@ -36,18 +90,9 @@ class PatientType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('markingNumber')
-            ->add('passportNumber')
-            ->add('appearance')
-            ->add('client', EntityType::class, [
-                'class' => Client::class,
-                'choice_label' => function (Client $client) {
-                    return $client->getName().' '.$client->getLastName();
-                },
-                'placeholder' => 'Select an owner',
-                'required' => true,
-            ])
-        ;
+            ->add('markingNumber', null, ['disabled' => $disabled])
+            ->add('passportNumber', null, ['disabled' => $disabled])
+            ->add('appearance', null, ['disabled' => $disabled]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
