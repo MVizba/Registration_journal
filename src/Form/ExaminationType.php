09d74c1f5292d0f082e39c4fd71ad5fd239a53cc
Marius Vizbras <marius.viz@gmail.com>
2024-12-18 12:51:51 +0200

    constrains

--- src/Form/ExaminationType.php
+++ src/Form/ExaminationType.php
@@ -6,16 +6,53 @@ use App\Entity\Examination;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ExaminationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('shortcut')
-            ->add('examinationName')
-            ->add('norms')
-            ->add('machine')
+            ->add('shortcut', null, [
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'The shortcut cannot be empty.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 10,
+                        'maxMessage' => 'The shortcut cannot be longer than {{ limit }} characters.',
+                    ]),
+                ],
+            ])
+            ->add('examinationName', null, [
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'The examination name cannot be empty.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 150,
+                        'maxMessage' => 'The examination name cannot be longer than {{ limit }} characters.',
+                    ]),
+                ],
+            ])
+            ->add('norms', null, [
+                'constraints' => [
+                    new Assert\Length([
+                        'max' => 200,
+                        'maxMessage' => 'Norms cannot be longer than {{ limit }} characters.',
+                    ]),
+                ],
+                'required' => false,
+            ])
+            ->add('machine', null, [
+                'constraints' => [
+                    new Assert\Length([
+                        'max' => 255,
+                        'maxMessage' => 'The machine name cannot be longer than {{ limit }} characters.',
+                    ]),
+                ],
+                'required' => false,
+            ])
         ;
     }
 
